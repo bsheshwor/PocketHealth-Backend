4e23f6aa77a148f48640d4f521ecfd0a6aaa4ae2
Bisheshwor Neupane <bn02433818@student.ku.edu.np>
2022-03-29 11:53:32 +0545

    Changed Loction file

--- pockethealth/account/location.py
+++ pockethealth/account/location.py
@@ -1,6 +1,58 @@
 from django.db import models
 from django.conf import settings
 
+class Status(models.Model):
+    STATUS_CODE = (('active','Active'),
+                   ('suspended','Suspended'),
+                   ('inactive','Inactive'),)
+    text = models.CharField(max_length=255, choices = STATUS_CODE)
+
+class OperationalStatus(models.Model):
+    OPERATIONAL_STATUS_CODE = (('C','Closed'),
+                               ('H','HouseKeeping'),
+                               ('I','Isolated'),
+                               ('K','Contaminated'),
+                               ('O','Occupied'),
+                               ('U','Unoccupied'),)
+    text = models.CharField(max_length=255, choices = OPERATIONAL_STATUS_CODE)
+
+class Mode(models.Model):
+    MODE_CODE = (('instance','Instance'),
+                 ('kind','Kind'),
+                 )
+
+    text = models.CharField(max_length=255, choices = MODE_CODE)
+
+class Type(models.Model):
+    TYPE_CODE = (('','Diagnostics or therapeutics unit'),
+                 ('','Cardiovascular diagnostics or therapeutics unit'),
+                 ('','Cardiac catheterization lab'),
+                 ('','Echocardiography lab'),
+                 ('','Gastroenterology diagnostics or therapeutics lab'),
+                 ('','Endoscopy lab'),
+                 ('','Radiology diagnostics or therapeutics unit'),
+                 ('','Radiation oncology unit'),
+                 ('','Neuroradiology unit'),
+                 ('','Hospital'),
+                 ('','Chronic Care Facility'),
+                 ('','Hospitals; General Acute Care Hospital'),
+                 ('','Military Hospital'),
+                 ('','Psychatric Care Facility'),
+                 ('','Rehabilitation hospital'),
+                 ('','addiction treatment center'),
+                 ('','intellectual impairment center'),
+                 ('','parents with adjustment difficulties center'),
+                 ('','physical impairment center'),
+                 ('','physical impairment - hearing center'),
+                 ('','physical impairment - motor skills center'),
+                 ('','physical impairment - visual skills center'),
+                 ('','youths with adjustment difficulties center'),
+                 ('','Hospital unit'),
+                 ('','Bone marrow transplant unit'),
+                 ('','Coronary care unit'),
+                 ('','Chest unit'),
+                 ('','Epilepsy unit'),
+                 )
 class PhysicalLocationType():
     PHYSICAL_CODE = (('si','Site'),
                      ('bu','Building'),
@@ -20,9 +72,11 @@ class PhysicalLocationType():
 
 class Position(models.Model):
     #WANT THIS TO BE STORED AUTOMATICALLY
-    longitude = decimal
-    latitude = decimal
-    altitude = decimal
+    #TODO: -->
+    # longitude = decimal
+    # latitude = decimal
+    # altitude = decimal
+    pass
 
 class HoursOfOperation(models.Model):
     DAYS_CODE = (('mon','Monday'),
